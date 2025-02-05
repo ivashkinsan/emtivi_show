7891cd28f9e84b0c80d537835b205cab1cc36c91
ivashkinsan <64753179+ivashkinsan@users.noreply.github.com>
2025-02-05 23:39:23 +0300

    new

---
+++ src/background_animate/background.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./background_animate.css";
+const Background = () =>{
+ return (<></>
+
+
+ )
+}
+
+export default Background;
---
+++ src/main_content/home.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./home.css"
+import Logo from "../main_content/home_logo.tsx";
+// import Background from "../background_animate/background.tsx";
+const Home =() =>{
+    return <section className="home">
+        <span>сайт группы emtivi</span>
+        <Logo />
+    </section>
+}
+export default Home
