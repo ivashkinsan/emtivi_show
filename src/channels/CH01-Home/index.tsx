7819af93aee98eff0889d405088f801364c8e887
jazzscriptdev <ivashkinsan@gmail.com>
2026-08-03 23:40:50 +0300

    mob_parallax

--- src/channels/CH01-Home/index.tsx
+++ src/channels/CH01-Home/index.tsx
@@ -1,9 +1,16 @@
 import styles from './Home.module.css';
 import { Hero } from './components/Hero';
+import { useParallax } from '../../hooks/useParallax';
 
 export const HomeChannel = () => {
+  const bgOffset = useParallax(30);
+  
   return (
     <div className={styles.container}>
+      <div 
+        className={styles.parallaxBackground}
+        style={{ transform: `translate(${bgOffset.x}px, ${bgOffset.y}px)` }}
+      />
       <Hero />
     </div>
   );
